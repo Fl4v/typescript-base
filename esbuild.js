41a199aeba10d137c0579e9ed629c22050a6ed9f
Flavius Stroe <stroe.flavius@gmail.com>
2022-01-19 09:59:09 +0000

    feat: update

--- esbuild.js
+++ esbuild.js
@@ -1,8 +1,11 @@
-require('esbuild').build({
-  entryPoints: ['./src/index.ts'],
-  outdir: './dist',
-  minify: true,
-  target: 'es2019',
-  platform: 'node',
-  format: 'cjs',
-});
+require('esbuild')
+  .build({
+    entryPoints: ['./src/index.ts'],
+    outdir: './dist',
+    minify: true,
+    target: 'es2020',
+    platform: 'node',
+  })
+  .catch((err) => {
+    throw err;
+  });
